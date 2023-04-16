268e2f8dd888b236734cbba9dac2ca6ce3395479
Agryo Araujo <agryostallion@gmail.com>
2023-04-16 03:20:57 -0300

    fisrt commit

---
+++ src/main/java/com/padroesprojetospringboot/PadroesProjetoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.padroesprojetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padroesprojetospringboot/PadroesProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padroesprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
